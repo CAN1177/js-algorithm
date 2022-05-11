e132fbce3e4e21fe11e5e1df3036e5f40fee80b1
CAN7 <39960526+CAN1177@users.noreply.github.com>
2022-05-11 16:46:22 +0800

    feat: tree

---
+++ imooc-demo/tree/postorder.js
@@ -0,0 +1,10 @@
+const bt = require("./bt");
+
+const postorder = (root) => {
+  if (!root) return;
+  postorder(root.left);
+  postorder(root.right);
+  console.log(root.val);
+};
+
+postorder(bt);
---
+++ imooc-demo/tree/preorder.js
@@ -0,0 +1,13 @@
+// 先序遍历
+const bt = require("./bt");
+
+const preorder = (root) => {
+  if (!root) {
+    return;
+  }
+  console.log(root.val);
+  preorder(root.left);
+  preorder(root.right);
+};
+
+ preorder(bt)
