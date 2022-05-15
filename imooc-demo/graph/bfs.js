c84eeca7c99ac9f2029f10683acc6a38bbb79cf4
CAN7 <39960526+CAN1177@users.noreply.github.com>
2022-05-15 11:23:19 +0800

    feat: leetcode 133

--- imooc-demo/graph/bfs.js
+++ imooc-demo/graph/bfs.js
@@ -59,54 +59,77 @@
 // };
 
 // 417太平洋大西洋水流问题
-var pacificAtlantic = function (heights) {
-  if (!heights || !heights[0]) {
-    return [];
-  }
+// var pacificAtlantic = function (heights) {
+//   if (!heights || !heights[0]) {
+//     return [];
+//   }
 
-  const m = heights.length;
-  const n = heights[0].length;
-  const flow1 = Array.from({ length: m }, () => new Array(n).fill(false));
-  const flow2 = Array.from({ length: m }, () => new Array(n).fill(false));
+//   const m = heights.length;
+//   const n = heights[0].length;
+//   const flow1 = Array.from({ length: m }, () => new Array(n).fill(false));
+//   const flow2 = Array.from({ length: m }, () => new Array(n).fill(false));
 
-  const dfs = (r, c, flow) => {
-    flow[r][c] = true;
-		[[r - 1, c], [r + 1, c], [r, c - 1], [r, c + 1]].forEach(
-      ([nr, nc]) => {
-        if (
-          // 保证在矩阵中
-          nr >= 0 &&
-          nr < m &&
-          nc >= 0 &&
-          nc < n &&
-          // 防止死循环
-          !flow[nr][nc] &&
-          // 保证逆流而上
-          heights[nr][nc] >= heights[r][c]
-        ) {
-          dfs(nr, nc, flow);
-        }
-      }
-    );
-  };
+//   const dfs = (r, c, flow) => {
+//     flow[r][c] = true;
+// 		[[r - 1, c], [r + 1, c], [r, c - 1], [r, c + 1]].forEach(
+//       ([nr, nc]) => {
+//         if (
+//           // 保证在矩阵中
+//           nr >= 0 &&
+//           nr < m &&
+//           nc >= 0 &&
+//           nc < n &&
+//           // 防止死循环
+//           !flow[nr][nc] &&
+//           // 保证逆流而上
+//           heights[nr][nc] >= heights[r][c]
+//         ) {
+//           dfs(nr, nc, flow);
+//         }
+//       }
+//     );
+//   };
 
-  // 沿着海岸线逆流而上
-  for (let r = 0; r < m; r += 1) {
-    dfs(r, 0, flow1);
-    dfs(r, n - 1, flow2);
+//   // 沿着海岸线逆流而上
+//   for (let r = 0; r < m; r += 1) {
+//     dfs(r, 0, flow1);
+//     dfs(r, n - 1, flow2);
+//   }
+//   for (let c = 0; c < n; c += 1) {
+//     dfs(0, c, flow1);
+//     dfs(m - 1, c, flow2);
+//   }
+//   // 收集坐标
+//   const res = [];
+//   for (let r = 0; r < m; r += 1) {
+//     for (let c = 0; c < n; c += 1) {
+//       if (flow1[r][c] && flow2[r][c]) {
+//         res.push([r, c]);
+//       }
+//     }
+//   }
+// 	return res;
+// };
+
+// 133克隆图
+
+var cloneGraph = function (node) {
+  if (!node) {
+    return;
   }
-  for (let c = 0; c < n; c += 1) {
-    dfs(0, c, flow1);
-    dfs(m - 1, c, flow2);
-  }
-  // 收集坐标
-  const res = [];
-  for (let r = 0; r < m; r += 1) {
-    for (let c = 0; c < n; c += 1) {
-      if (flow1[r][c] && flow2[r][c]) {
-        res.push([r, c]);
-      }
-    }
-  }
-	return res;
+  const visited = new Map();
+  // const dfs = (n) => {
+  //   // console.log(n.val);
+  //   const nCopy = new Node(n.val);
+  //   visited.set(n, nCopy);
+  //   n?.neighbors.forEach((ne) => {
+  //     if (!visited.has(ne)) {
+  //       dfs(ne);
+  //     }
+  //     nCopy.neighbors.push(visited.get(ne));
+  //   });
+  // };
+  // dfs(node);
+
+  return visited.get(node);
 };
